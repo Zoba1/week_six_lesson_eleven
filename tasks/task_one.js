b792349592d221ddc86a28e733fb64e378c36bf0
adigwechizoba181@gmail.com <adigwechizoba181@gmail.com>
2023-10-31 14:39:34 +0000

    modified

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -20,57 +20,101 @@
 // })
 
 
-const username= document.querySelector('#name')
-const userage= document.querySelector('#age')
-const submitBtn= document.querySelector('#submit')
-const container= document.querySelector('.display')
+// const username= document.querySelector('#name')
+// const userage= document.querySelector('#age')
+// const submitBtn= document.querySelector('#submit')
+// const container= document.querySelector('.display')
 
-submitBtn.addEventListener('click', ()=> {
-    if(username.value && userage.value){
-        displayItems(username.value, userage.value)
-        updateLocal()
+// submitBtn.addEventListener('click', ()=> {
+//     if(username.value && userage.value){
+//         displayItems(username.value, userage.value)
+//         updateLocal()
+//     }
+//     username.value=''
+//     userage.value=''
+// })
+
+
+// function updateLocal() {
+//     const liElements= container.querySelectorAll('li')
+//     const itemsToSave= Array.from(liElements).map(info => {
+//         const name= info.querySelector('.name').textContent
+//         const age= info.querySelector('.age').textContent
+//         return {name, age}
+//     })
+
+//     localStorage.setItem('zobs', JSON.stringify(itemsToSave))
+
+// }
+
+// function loadFromLocal() {
+//     const savedItems=JSON.parse(localStorage.getItem('zobs')) || []
+//     savedItems.forEach(item => {
+//         displayItems(item.name, item.age)
+//     })
+
+// }
+
+// loadFromLocal()
+
+// function displayItems(name, age) {
+//     const li= document.createElement('li')
+//     li.innerHTML=`<span class='name'>${name}</span> <span class='age'>${age}</span> <button class="del">Del</button>`
+//     container.append(li)
+// }
+
+// const delBtn= container.querySelectorAll('.del')
+
+// delBtn.forEach(btn=> {
+//     btn.addEventListener('click',(e) => {
+//         e.target.parentElement.remove()
+//         updateLocal()
+//     })
+// })
+
+
+
+class FoodApp {
+    constructor() {
+      this.foodList = [];
+      this.firstSetup();
     }
-    username.value=''
-    userage.value=''
-})
-
-
-function updateLocal() {
-    const liElements= container.querySelectorAll('li')
-    const itemsToSave= Array.from(liElements).map(info => {
-        const name= info.querySelector('.name').textContent
-        const age= info.querySelector('.age').textContent
-        return {name, age}
-    })
-
-    localStorage.setItem('zobs', JSON.stringify(itemsToSave))
-
-}
-
-function loadFromLocal() {
-    const savedItems=JSON.parse(localStorage.getItem('zobs')) || []
-    savedItems.forEach(item => {
-        displayItems(item.name, item.age)
-    })
-
-}
-
-loadFromLocal()
-
-function displayItems(name, age) {
-    const li= document.createElement('li')
-    li.innerHTML=`<span class='name'>${name}</span> <span class='age'>${age}</span> <button class="del">Del</button>`
-    container.append(li)
-}
-
-const delBtn= container.querySelectorAll('.del')
-
-delBtn.forEach(btn=> {
-    btn.addEventListener('click',(e) => {
-        e.target.parentElement.remove()
-        updateLocal()
-    })
-})
-
-
-
+    firstSetup() {
+      this.loadFromLocalStorage();
+      this.firstFoodList();
+      this.setupEventListeners();
+    }
+    setupEventListeners() {
+      const addFoodButton = document.getElementById('addFood');
+      addFoodButton.addEventListener('click', () => this.addFood());
+    }
+    addFood() {
+      const foodNameInput = document.getElementById('foodName');
+      const foodName = foodNameInput.value.trim();
+      if (foodName !== '') {
+        this.foodList.push(foodName);
+        foodNameInput.value = '';
+        this.saveToLocalStorage();
+        this.firstFoodList();
+      }
+    }
+    firstFoodList() {
+      const foodListContainer = document.getElementById('foodList');
+      foodListContainer.innerHTML = '';
+      this.foodList.forEach(food => {
+        const listItem = document.createElement('li');
+        listItem.textContent = food;
+        foodListContainer.appendChild(listItem);
+      });
+    }
+    saveToLocalStorage() {
+      localStorage.setItem('foodList', JSON.stringify(this.foodList));
+    }
+    loadFromLocalStorage() {
+      const savedFoodList = localStorage.getItem('foodList');
+      if (savedFoodList) {
+        this.foodList = JSON.parse(savedFoodList);
+      }
+    }
+  }
+  const app = new FoodApp();
